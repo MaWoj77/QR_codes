c629ba4437f8c34e33e234b3410f859224e3b284
MaWoj77 <wojewoda.m14@gmail.com>
2025-07-15 08:52:57 +0200

    update

--- QR_codes.py
+++ QR_codes.py
@@ -1,6 +1,8 @@
 import os
 import qrcode
 
-img = qrcode.make("https://www.linkedin.com/in/maria-wojewoda/")
+img_linkedin = qrcode.make("https://www.linkedin.com/in/maria-wojewoda/")
+img_github = qrcode.make("https://github.com/MaWoj77")
 
-img.save("LinkedIn_qr.png", "PNG")
+img_linkedin.save("LinkedIn_qr.png", "PNG")
+img_github.save("GitHub_qr.png", "PNG")
